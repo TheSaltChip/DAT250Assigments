1b8b8d98e4c31dcbdf3955889811026880bd9988
Sebastian Misje Jonassen <seb2500@gmail.com>
2022-10-08 13:25:49 +0200

    Converted UserService to an interface, and converted the original file to UserServiceImpl

--- AssignmentB/src/main/java/no/hvl/dat250/jpa/assignmentB/service/UserService.java
+++ AssignmentB/src/main/java/no/hvl/dat250/jpa/assignmentB/service/UserService.java
@@ -1,86 +1,33 @@
 package no.hvl.dat250.jpa.assignmentB.service;
 
-import no.hvl.dat250.jpa.assignmentB.dao.client.UserRepository;
 import no.hvl.dat250.jpa.assignmentB.models.Poll;
 import no.hvl.dat250.jpa.assignmentB.models.Role;
 import no.hvl.dat250.jpa.assignmentB.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
-@Service
-public class UserService {
-    private final UserRepository userRepository;
+/**
+ * Interface for user services
+ *
+ * @author Sebastian
+ */
+public interface UserService {
+    List<User> getAllUsers();
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    User getUserByUsername(String username);
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+    User saveUser(User user);
 
-    public User getUserByUsername(String username) {
-        return userRepository.findById(username).orElse(null);
-    }
+    User updateUser(String username, User user);
 
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
+    Set<Poll> getOwnedPollsFromUser(String username);
 
-    public User updateUser(String username, User user) {
-        User updatedUser = userRepository.findById(username).orElse(null);
-        if (updatedUser == null) {
-            return null;
-        }
+    Poll addPollToUser(String username, Poll poll);
 
-        updatedUser.setFirstname(user.getFirstname());
-        updatedUser.setLastname(user.getLastname());
-        updatedUser.setEmail(user.getEmail());
-        updatedUser.setPassword(user.getPassword());
-        updatedUser.setRole(user.getRole());
+    void deleteUser(String username);
 
-        return userRepository.save(updatedUser);
-    }
+    User changeRoleOfUser(String username, Role role);
 
-    public Set<Poll> getOwnedPollsFromUser(String username) {
-        User c = userRepository.findById(username).orElse(null);
-        if (c == null) return null;
-        return c.getOwnedPolls();
-    }
-
-    public Poll addPollToUser(String username, Poll poll) {
-        User c = userRepository.findById(username).orElse(null);
-
-        if (c == null) return null;
-
-        c.getOwnedPolls().add(poll);
-        userRepository.save(c);
-
-        return poll;
-    }
-
-    public void deleteUser(String username) {
-        userRepository.deleteById(username);
-    }
-
-    public User changeRoleOfUser(String username, Role role){
-        User user = userRepository.findById(username).orElse(null);
-
-        if(user == null) return null;
-
-        user.setRole(role);
-
-        return user;
-    }
-
-    public Role getRoleOfUser(String username) {
-        User user = userRepository.findById(username).orElse(null);
-
-        return user == null ? null : user.getRole();
-    }
+    Role getRoleOfUser(String username);
 }
---
+++ AssignmentB/src/main/java/no/hvl/dat250/jpa/assignmentB/service/UserServiceImpl.java
@@ -0,0 +1,94 @@
+package no.hvl.dat250.jpa.assignmentB.service;
+
+import no.hvl.dat250.jpa.assignmentB.dao.client.UserRepository;
+import no.hvl.dat250.jpa.assignmentB.models.Poll;
+import no.hvl.dat250.jpa.assignmentB.models.Role;
+import no.hvl.dat250.jpa.assignmentB.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findById(username).orElse(null);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(String username, User user) {
+        User updatedUser = userRepository.findById(username).orElse(null);
+        if (updatedUser == null) {
+            return null;
+        }
+
+        updatedUser.setFirstname(user.getFirstname());
+        updatedUser.setLastname(user.getLastname());
+        updatedUser.setEmail(user.getEmail());
+        updatedUser.setPassword(user.getPassword());
+        updatedUser.setRole(user.getRole());
+
+        return userRepository.save(updatedUser);
+    }
+
+    @Override
+    public Set<Poll> getOwnedPollsFromUser(String username) {
+        User c = userRepository.findById(username).orElse(null);
+        if (c == null) return null;
+        return c.getOwnedPolls();
+    }
+
+    @Override
+    public Poll addPollToUser(String username, Poll poll) {
+        User c = userRepository.findById(username).orElse(null);
+
+        if (c == null) return null;
+
+        c.getOwnedPolls().add(poll);
+        userRepository.save(c);
+
+        return poll;
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        userRepository.deleteById(username);
+    }
+
+    @Override
+    public User changeRoleOfUser(String username, Role role){
+        User user = userRepository.findById(username).orElse(null);
+
+        if(user == null) return null;
+
+        user.setRole(role);
+
+        return user;
+    }
+
+    @Override
+    public Role getRoleOfUser(String username) {
+        User user = userRepository.findById(username).orElse(null);
+
+        return user == null ? null : user.getRole();
+    }
+}
