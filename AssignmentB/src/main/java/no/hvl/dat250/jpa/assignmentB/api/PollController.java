d32531a659cfc9d581725827708a7a537b3e0f2d
Sebastian Misje Jonassen <seb2500@gmail.com>
2022-10-09 14:15:26 +0200

    filled this thing out

--- AssignmentB/src/main/java/no/hvl/dat250/jpa/assignmentB/api/PollController.java
+++ AssignmentB/src/main/java/no/hvl/dat250/jpa/assignmentB/api/PollController.java
@@ -1,10 +1,14 @@
 package no.hvl.dat250.jpa.assignmentB.api;
 
+import no.hvl.dat250.jpa.assignmentB.api.pojo.DeviceVotes;
+import no.hvl.dat250.jpa.assignmentB.api.pojo.UserVote;
 import no.hvl.dat250.jpa.assignmentB.models.User;
 import no.hvl.dat250.jpa.assignmentB.models.Poll;
 import no.hvl.dat250.jpa.assignmentB.models.TimeLimitPoll;
-import no.hvl.dat250.jpa.assignmentB.service.PollService;
+import no.hvl.dat250.jpa.assignmentB.service.poll.PollService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +16,7 @@ import java.time.LocalDateTime;
 
 @Controller
 public class PollController {
-/*
+
     private final PollService pollService;
 
     @Autowired
@@ -21,57 +25,59 @@ public class PollController {
     }
 
     @GetMapping("/poll/{pollId}")
-    public Poll findById(@PathVariable int pollId){
+    public Poll findById(@PathVariable Long pollId) {
         return pollService.findById(pollId);
     }
 
     @GetMapping("/user/{pollId}")
-    public User getOwner(@PathVariable int pollId) {
+    public User getOwner(@PathVariable Long pollId) {
         return pollService.getOwner(pollId);
     }
 
     @PutMapping("/votePage/{pollId}")
-    public Poll updateVote(@RequestBody boolean vote,@PathVariable int pollId){
-        return pollService.updateVote(vote,pollId);
+    public Poll updateVote(@RequestBody UserVote test, @PathVariable Long pollId) {
+        return pollService.updateVote(test.isVote(), test.getUsername(), pollId);
     }
 
     @PutMapping("/voteDevice/{pollId}")
-    public Poll updateVote(@RequestBody int yes,@RequestBody int no,@PathVariable int pollId) {
-        return pollService.updateVote(yes,no,pollId);
+    public Poll updateVote(@RequestBody DeviceVotes deviceVotes, @PathVariable Long pollId) {
+        return pollService.updateVote(deviceVotes.getDeviceId(), deviceVotes.getYes(), deviceVotes.getNo(), pollId);
     }
 
     @PutMapping("/poll/update")
     public void updatePoll(@RequestBody Poll poll) {
-        pollService.updatePoll(Integer.parseInt(""+poll.getId()),poll.getName(),poll.getTheme());
+        pollService.updatePoll(poll);
     }
 
-    @PutMapping("/close/{pollId}")
-    public Poll closePoll(@PathVariable int pollId) {
+    @PutMapping("/poll/close/{pollId}")
+    public Poll closePoll(@PathVariable Long pollId) {
         return pollService.closePoll(pollId);
     }
 
-    @PutMapping("/open/{pollId}")
-    public Poll openPoll(@PathVariable int pollId) {
+    @PutMapping("/poll/open/{pollId}")
+    public Poll openPoll(@PathVariable Long pollId) {
         return pollService.openPoll(pollId);
     }
 
     @PutMapping("/poll/time/{pollId}")
-    public Poll updateTime(@PathVariable int pollId,@RequestBody LocalDateTime startDate,@RequestBody LocalDateTime endDate) {
-       return pollService.updateTime(pollId,startDate,endDate);
+    public Poll updateTime(@PathVariable Long pollId, @RequestBody LocalDateTime startDate, @RequestBody LocalDateTime endDate) {
+        return pollService.updateTime(pollId, startDate, endDate);
     }
 
     @PostMapping("poll")
     public Poll createPoll(@RequestBody Poll poll) {
-        return pollService.createPoll(poll.getName(),poll.getTheme(), poll.getPrivate(),poll.getCreatedDate(),poll.getOwner());
+        return pollService.createPoll(poll);
     }
 
     @PostMapping("timedPoll")
     public TimeLimitPoll createTimeLimitPoll(@RequestBody TimeLimitPoll poll) {
-        return pollService.createTimeLimitPoll(poll.getName(),poll.getTheme(),poll.getPrivate(),poll.getCreatedDate(),poll.getOwner(),poll.getStartDate(),poll.getEndDate());
+        return pollService.createTimeLimitPoll(poll);
     }
 
     @DeleteMapping("/deletePoll/{pollId}")
-    public Poll deletePoll(@PathVariable int pollId) {
-        return pollService.deletePoll(pollId);
-    }*/
+    public ResponseEntity<HttpStatus> deletePoll(@PathVariable Long pollId) {
+        pollService.deletePoll(pollId);
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 }
