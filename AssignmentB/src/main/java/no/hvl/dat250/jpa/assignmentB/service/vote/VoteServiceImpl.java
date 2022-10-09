f083ff41c10517e7e29fd48e4f652be09d150246
Sebastian Misje Jonassen <seb2500@gmail.com>
2022-10-09 14:17:46 +0200

    Create VoteServiceImpl.java

---
+++ AssignmentB/src/main/java/no/hvl/dat250/jpa/assignmentB/service/vote/VoteServiceImpl.java
@@ -0,0 +1,42 @@
+package no.hvl.dat250.jpa.assignmentB.service.vote;
+
+import no.hvl.dat250.jpa.assignmentB.models.Poll;
+import no.hvl.dat250.jpa.assignmentB.models.User;
+import no.hvl.dat250.jpa.assignmentB.models.Vote;
+import no.hvl.dat250.jpa.assignmentB.repository.poll.PollRepository;
+import no.hvl.dat250.jpa.assignmentB.repository.user.UserRepository;
+import no.hvl.dat250.jpa.assignmentB.repository.vote.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VoteServiceImpl implements VoteService {
+    private final VoteRepository voteRepository;
+    private final PollRepository pollRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public VoteServiceImpl(VoteRepository repository, PollRepository pollRepository, UserRepository userRepository) {
+        this.voteRepository = repository;
+        this.pollRepository = pollRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<Vote> getAllVotes() {
+        return voteRepository.findAll();
+    }
+
+    public List<Vote> getAllVotesFromPoll(Long pollId) {
+        Poll p = pollRepository.getReferenceById(pollId);
+        return voteRepository.findAllByPoll(p);
+    }
+
+    public List<Vote> getAllVotesFromUser(String username){
+        User u = userRepository.getReferenceById(username);
+
+        return voteRepository.findAllByUser(u);
+    }
+
+}
