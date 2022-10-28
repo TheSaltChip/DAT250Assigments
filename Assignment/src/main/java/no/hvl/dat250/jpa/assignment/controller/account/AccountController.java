4c5b40933088735e34bfb124313df79d728bcdfd
Sebastian Misje Jonassen <seb2500@gmail.com>
2022-10-28 13:09:34 +0200

    created /account folder

--- Assignment/src/main/java/no/hvl/dat250/jpa/assignment/controller/account/AccountController.java
+++ Assignment/src/main/java/no/hvl/dat250/jpa/assignment/controller/account/AccountController.java
@@ -1,4 +1,4 @@
-package no.hvl.dat250.jpa.assignment.controller;
+package no.hvl.dat250.jpa.assignment.controller.account;
 
 import no.hvl.dat250.jpa.assignment.authentication.facade.IAuthenticationFacade;
 import no.hvl.dat250.jpa.assignment.models.User;
