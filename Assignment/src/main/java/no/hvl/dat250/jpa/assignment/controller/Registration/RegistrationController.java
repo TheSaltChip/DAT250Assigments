1741b70bb2707442d99fe290125847281f4180a7
OysteinKvilhaugsvik <54100417+OysteinKvilhaugsvik@users.noreply.github.com>
2022-10-30 16:36:22 +0100

    register new user method implemented

--- Assignment/src/main/java/no/hvl/dat250/jpa/assignment/controller/Registration/RegistrationController.java
+++ Assignment/src/main/java/no/hvl/dat250/jpa/assignment/controller/Registration/RegistrationController.java
@@ -1,21 +1,76 @@
 package no.hvl.dat250.jpa.assignment.controller.Registration;
 
+import lombok.NonNull;
+import no.hvl.dat250.jpa.assignment.models.Role;
+import no.hvl.dat250.jpa.assignment.models.User;
 import no.hvl.dat250.jpa.assignment.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.validation.Valid;
+
 
 @Controller
 public class RegistrationController {
 
     private UserService userService;
 
-    @GetMapping(value="/register")
-    public String registerPage(Model model){
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
 
-        UserDto user = new UserDto();
-        model.addAttribute("user", user);
+    @GetMapping(value="/register")
+    public String registerPage(WebRequest request, Model model){
+
+        UserData userData = new UserData();
+        model.addAttribute("userData", userData);
 
         return "/register";
+    }
+
+    @PostMapping(value="/register")
+    public String registerUserAccount(@Valid UserData userData, BindingResult bindingResult, Model model) {
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("registrationForm", userData);
+            return "/register";
+        }
+        try {
+            registerNewUser(userData);
+        } catch (Exception e) {
+            bindingResult.rejectValue("username", "userData.username","Existing Account");
+            model.addAttribute("registrationForm", userData);
+            return "/register";
+        }
+            return "/login";
+    }
+
+    public void registerNewUser(@NonNull UserData userData) throws Exception {
+
+        if(existingUserCheck(userData.getUsername())) {
+            throw new Exception("Account with this username already exists");
+        }
+
+        User user = new User();
+
+        user.setFirstname(userData.getFirstName());
+        user.setLastname(userData.getLastName());
+        user.setUsername(userData.getUsername());
+        user.setPassword(userData.getPassword());
+        user.setEmail(userData.getEmail());
+        user.setRole(Role.Regular);
+
+        userService.saveUser(user);
+    }
+
+    public boolean existingUserCheck(String username) {
+        //userService.getUserByUsername(username) != null;
+        return false;
     }
 }
---
+++ Assignment/src/main/java/no/hvl/dat250/jpa/assignment/controller/Registration/UserData.java
@@ -0,0 +1,31 @@
+package no.hvl.dat250.jpa.assignment.controller.Registration;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import no.hvl.dat250.jpa.assignment.models.Role;
+
+import javax.validation.constraints.NotEmpty;
+
+@Setter
+@Getter
+public class UserData {
+    @NotEmpty(message = "First name cannot be empty")
+    private String firstName;
+
+    @NotEmpty(message = "Last name cannot be empty")
+    private String lastName;
+
+    @NotEmpty(message = "User name cannot be empty")
+    private String username;
+
+    @NotNull
+    private Role role;
+
+    @NotEmpty(message = "Password cannot be empty")
+    private String password;
+
+    @NotEmpty(message = "Email cannot be empty")
+    private String email;
+
+}
