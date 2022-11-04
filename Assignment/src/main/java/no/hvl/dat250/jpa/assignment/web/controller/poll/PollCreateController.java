69e241c577174e4ed835095ee7140ff1cc924b5b
Sebastian Misje Jonassen <seb2500@gmail.com>
2022-11-05 00:09:46 +0100

    Done with poll creation controller?

--- Assignment/src/main/java/no/hvl/dat250/jpa/assignment/web/controller/poll/PollCreateController.java
+++ Assignment/src/main/java/no/hvl/dat250/jpa/assignment/web/controller/poll/PollCreateController.java
@@ -5,11 +5,12 @@ import no.hvl.dat250.jpa.assignment.models.Poll;
 import no.hvl.dat250.jpa.assignment.models.User;
 import no.hvl.dat250.jpa.assignment.service.poll.PollService;
 import no.hvl.dat250.jpa.assignment.service.user.UserService;
-import no.hvl.dat250.jpa.assignment.web.formObject.PollForm;
+import no.hvl.dat250.jpa.assignment.web.formObject.PollCreateForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,22 +31,30 @@ public class PollCreateController {
     }
 
     @GetMapping(value = "/poll/create")
-    public String showForm(PollForm pollForm) {
+    public String showForm(Model model) {
+        model.addAttribute("pollForm", new PollCreateForm());
+
         return "poll/pollcreation";
     }
 
     @PostMapping(value = "/poll/create")
-    public String validateInput(@Valid PollForm pollForm, BindingResult bindResult) {
+    public String validateInput(@Valid PollCreateForm pollCreateForm, BindingResult bindResult) {
         Authentication authentication = authenticationFacade.getAuthentication();
 
-        if (bindResult.hasErrors() || authentication instanceof AnonymousAuthenticationToken) {
-            return "poll/pollcreation";
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return "redirect:/login";
         }
 
+        if (bindResult.hasErrors()) {
+            bindResult.getAllErrors().forEach(e -> System.out.println(e.toString()));
+            return "redirect:/poll/pollcreation";
+        }
 
         User u = userService.getUserByUsername(authentication.getName());
 
-        Poll poll = new Poll(pollForm.getQuestion(), pollForm.getTheme(), pollForm.getIsPrivate(), u);
+        Poll poll = new Poll(pollCreateForm.getQuestion(), pollCreateForm.getTheme(), pollCreateForm.getIsPrivate(), u);
+
+        pollService.createPoll(poll);
 
         return "redirect:/poll/" + poll.getId();
     }
