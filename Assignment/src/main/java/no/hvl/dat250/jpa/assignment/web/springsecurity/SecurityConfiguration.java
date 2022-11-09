77a9e0fcc9f60406911db9487f45eb08b1a24865
Sebastian Misje Jonassen <seb2500@gmail.com>
2022-11-09 11:29:50 +0100

    CORS stuff and opened to api for all requests

--- Assignment/src/main/java/no/hvl/dat250/jpa/assignment/web/springsecurity/SecurityConfiguration.java
+++ Assignment/src/main/java/no/hvl/dat250/jpa/assignment/web/springsecurity/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 package no.hvl.dat250.jpa.assignment.web.springsecurity;
 
-import no.hvl.dat250.jpa.assignment.models.Role;
+import no.hvl.dat250.jpa.assignment.models.user.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -26,9 +31,10 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors().and().csrf().disable()
                 .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/", "/css/**", "/account/register","/poll/{id}/result").permitAll()
+                        .antMatchers("/", "/css/**", "/account/register", "/poll/{id}/result").permitAll()
+                        .antMatchers("/api/**").permitAll()
                         .antMatchers("/hello").hasRole(Role.Admin.toString())
                         .anyRequest().authenticated()
                 )
