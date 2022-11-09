2584085463ffcc595f358ca4ceed2901dcebda6a
Sebastian Misje Jonassen <35291227+TheSaltChip@users.noreply.github.com>
2022-11-09 12:00:06 +0100

    Delete DatasourceCredentials.java

--- Assignment/src/main/java/no/hvl/dat250/jpa/assignment/config/datasource/DatasourceCredentials.java
+++
@@ -1,8 +0,0 @@
-package no.hvl.dat250.jpa.assignment.config.datasource;
-
-public class DatasourceCredentials {
-
-    public static final String USERNAME = "postgres";
-    public static final String PASSWORD = "pass";
-
-}
