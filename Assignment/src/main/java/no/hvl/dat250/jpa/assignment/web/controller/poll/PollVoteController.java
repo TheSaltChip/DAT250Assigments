e9ea5757e7cf68838d13365365a8083961959c46
hvl584967 <54099528+hvl584967@users.noreply.github.com>
2022-11-09 20:05:41 +0100

    Improvements

--- Assignment/src/main/java/no/hvl/dat250/jpa/assignment/web/controller/poll/PollVoteController.java
+++ Assignment/src/main/java/no/hvl/dat250/jpa/assignment/web/controller/poll/PollVoteController.java
@@ -3,11 +3,9 @@ package no.hvl.dat250.jpa.assignment.web.controller.poll;
 import no.hvl.dat250.jpa.assignment.authentication.facade.AuthenticationFacade;
 import no.hvl.dat250.jpa.assignment.models.poll.Poll;
 import no.hvl.dat250.jpa.assignment.models.user.User;
-import no.hvl.dat250.jpa.assignment.models.vote.UserVote;
-import no.hvl.dat250.jpa.assignment.models.vote.AnonymousVote;
-import no.hvl.dat250.jpa.assignment.models.vote.DeviceVote;
 import no.hvl.dat250.jpa.assignment.service.poll.PollService;
 import no.hvl.dat250.jpa.assignment.service.user.UserService;
+import no.hvl.dat250.jpa.assignment.service.vote.VoteService;
 import no.hvl.dat250.jpa.assignment.web.formObject.VoteForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -24,6 +22,7 @@ public class PollVoteController {
     private PollService pollService;
     private AuthenticationFacade authenticationFacade;
     private UserService userService;
+    private VoteService voteService;
 
     @Autowired
     public PollVoteController(PollService pollService, AuthenticationFacade authenticationFacade, UserService userService) {
@@ -35,11 +34,15 @@ public class PollVoteController {
     @GetMapping(value = "/poll/{id}/vote")
     public String votePage(@PathVariable Long id, Model model){
         Poll poll = pollService.findById(id);
+        Authentication authentication = authenticationFacade.getAuthentication();
         if(poll.getIsPrivate()) {
-            Authentication authentication = authenticationFacade.getAuthentication();
             if (authentication instanceof AnonymousAuthenticationToken) {
                 return "redirect:/login";
             }
+        }
+        if(!(authentication instanceof AnonymousAuthenticationToken)){
+            System.out.println(authentication);
+            System.out.println(voteService.hasUserVotedInPoll(authentication.getName(),id));
         }
         model.addAttribute("poll",poll);
         model.addAttribute("voteform",new VoteForm());
@@ -50,31 +53,24 @@ public class PollVoteController {
     public String voteRegister(@PathVariable Long id, VoteForm voteform){
         Poll poll = pollService.findById(id);
         Authentication authentication = authenticationFacade.getAuthentication();
+        User user = null;
+        if(!(authentication instanceof AnonymousAuthenticationToken)){
+            user = userService.getUserByUsername(authentication.getName());
+        }
         if(poll.getIsPrivate()){
             if (authentication instanceof AnonymousAuthenticationToken) {
                 return "redirect:login";
             }
-            addUserVote(authentication,voteform,poll);
+            pollService.updateUserVote(voteform.isVote(), user.getUsername(), id);
+        }else {
+            if (user != null){
+                pollService.updateUserVote(voteform.isVote(), user.getUsername(), id);
+            }else{
+                pollService.updateAnonymousVote(poll, voteform.isVote());
+            }
         }
 
-        if (!(authentication instanceof AnonymousAuthenticationToken)){
-            addUserVote(authentication,voteform,poll);
-        }else{
-            pollService.updateAnonymousVote(poll, voteform.isVote());
-        }
-
-        String string = "redirect:/poll/"+id+"/result";
-        return string;
-    }
-
-    private void addUserVote(Authentication authentication,VoteForm voteform,Poll poll){
-        User user = userService.getUserByUsername(authentication.getName());
-
-        if(voteform.isVote()){
-            pollService.updateUserVote(true, user.getUsername(), poll.getId());
-        }else{
-            pollService.updateUserVote(false, user.getUsername(), poll.getId());
-        }
+        return "redirect:result";
     }
 
 }
