5f3983c0490b3a401c864c31bf3231a8e8998eef
Sebastian Misje Jonassen <seb2500@gmail.com>
2022-11-11 22:19:39 +0100

    Dweet pojo

---
+++ Assignment/src/main/java/no/hvl/dat250/jpa/assignment/service/dweet/DweetPoll.java
@@ -0,0 +1,10 @@
+package no.hvl.dat250.jpa.assignment.service.dweet;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DweetPoll {
+    private String status;
+}
